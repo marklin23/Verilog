b1df2fb2c8f9e0461ccc191cb7d022db67ee0053
林于凱 <marklin23@hotmail.com>
2017-03-26 22:47:11 +0800

    Add files via upload

--- i2c_slave.v
+++ i2c_slave.v
@@ -1,166 +1,265 @@
-module i2c_mark_rw(
-input   RESETn, SYSTEM_CLK,
-input   SCL,
-inout   SDA
+///////////////////////////////////////////////////////
+// edit by Mark 2016/12/26
+// Support Repeat Start and single read
+///////////////////////////////////////////////////////
+
+
+module i2c_slave#(
+	parameter slave_addr = 7'b010_0011)
+(
+	input   RESETn, SYSTEM_CLK,
+	// I2C_Interface
+	input   SCL,
+	inout   SDA,
+	input	[7:0]tx_data,
+	output	[6:0]rx_address,	
+	output	[7:0]rx_data,
+	output	[7:0]rx_offset,
+	output	owrite_en,
+	output	oread_en
 );
-
-parameter slave_addr = 7'b010_0011;
-reg [7:0] read_data = 7'b0101_0011;
-
-reg [3:0]CS;
-reg [3:0]NS;
-//counter
-reg [4:0]bitcnt;
-reg [4:0]bitcnt_o;
-
-//data bus
-reg [7:0]data_recived;
-reg  RW ;
-reg [6:0]device_addr_o;
-reg [7:0]register_addr_o;
-reg [7:0]data_o;
-reg sda_o;
-
-//pipe dectect 
-reg [1:0] sda_pipe ;
-reg [1:0] scl_pipe ;
-
-//reg [7:0] sda_data;
-parameter idle				=4'b0000;
-parameter device_addr		=4'b0010;
-parameter device_addr_ack	=4'b0011;
-parameter register_addr		=4'b0100;
-parameter register_addr_ack	=4'b0101;
-parameter data_write		=4'b0110;
-parameter data_write_ack	=4'b0111;
-parameter data_read			=4'b1000;
-parameter data_read_ack		=4'b1001;
-parameter stop				=4'b1010;
-
-
-
-
-assign SDA         = sda_o;
-
-always @(posedge SYSTEM_CLK)
-	begin
-	if (!RESETn)
-		bitcnt_o <= 5'd8;
-	else if(scl_pipe==2'b01)
-		begin
-			if(bitcnt_o == 5'd0)
-				bitcnt_o <= 5'd8;
-			else 
-				begin
-					bitcnt_o <= bitcnt_o-5'd1;
-					data_recived <= {data_recived[6:0],SDA};
-				end
-		end
-	else if((sda_pipe==2'b01 && scl_pipe==2'b11)|| (sda_pipe==2'b10 && scl_pipe==2'b11) )
-		bitcnt_o <= 5'd8;
-	end
-		
-	
+	reg	[4:0]	state;
+	reg	[10:0]	decode_data;
+	reg	[4:0]	count;
+	reg	[1:0]	sda_pipe;
+	reg	[1:0]	scl_pipe;
+	reg	[6:0]	address_data;
+	reg			RW_data		;
+	reg	[7:0]	offset_data	;
+	reg	[7:0]	write_data	;
+	reg			tx_shift	;
+	reg			sda_o;
+	reg	[7:0]	shift_reg;
+	assign	load	=	oread_en;
+	assign	read	=	(RW_data==1'b1)?1'b1:1'b0;
+	assign	write	=	(RW_data==1'b0)?1'b1:1'b0;
+//////////////////////////////////////////////////////////////////////
 //creat pipe
-always @(posedge SYSTEM_CLK)
-	begin
-		sda_pipe<={sda_pipe[0],SDA};
-		scl_pipe<={scl_pipe[0],SCL};
-	end
+//////////////////////////////////////////////////////////////////////
+
 	
-//state machine
-always @(posedge SYSTEM_CLK)
-    begin
-		CS <= (!RESETn)? idle : NS ;
-    end	
-
-always @(*)
+always @(posedge SYSTEM_CLK or negedge RESETn)
 	begin
-		 bitcnt <= bitcnt_o;
-		case(CS)
-		idle: //00
-					begin
-						NS 					<= (sda_pipe==2'b10 && scl_pipe==2'b11) ? device_addr : idle ; //START 
-						device_addr_o		<= 7'dx;
-						register_addr_o		<= 8'dx;
-						data_o   			<= 8'dx;
-						sda_o				<= 1'bz;
-						data_recived		<= 8'bx;
-						bitcnt				<= 5'd8;
-						RW 					<= 1'bx;
-						
-					end
-		device_addr: //10
-					begin
+		sda_pipe	<=	!RESETn	?	1'b0	:	{sda_pipe[0],SDA};
+		scl_pipe	<=	!RESETn	?	1'b0	:	{scl_pipe[0],SCL};
+	end
 
-						RW 					<= ((bitcnt == 0) && scl_pipe==2'b10 ) ? data_recived[0] : RW ;
-						device_addr_o		<= ((bitcnt == 0&& scl_pipe==2'b10 )) ? data_recived[7:1] : device_addr_o ;
-						NS					<= (bitcnt == 0 && scl_pipe==2'b10) ? device_addr_ack : device_addr ;
-						
-					end
-		device_addr_ack: //11
-					begin
-						NS 					<= (device_addr_o == slave_addr)?
-												((bitcnt == 8 && scl_pipe==2'b10 )?(RW ? data_read:register_addr):device_addr_ack)
-												:idle;
-						sda_o				<= (device_addr_o == slave_addr)?((bitcnt == 8 && scl_pipe==2'b10 )?1'bz:1'b0):1'bz ;
-					end
-		register_addr: //100
-					begin
-						register_addr_o		<= ((bitcnt == 0)&& scl_pipe==2'b10 ) ? data_recived[6:0] : register_addr_o ;
-						NS					<= (bitcnt == 0 && scl_pipe==2'b10 ) ? register_addr_ack : register_addr ;
-					end
-		register_addr_ack: //101
-					begin
+//////////////////////////////////////////////////////////////////////
+//bit-counter
+//////////////////////////////////////////////////////////////////////
 
-						sda_o				<= (device_addr_o == slave_addr)?((bitcnt == 8 && scl_pipe==2'b10 )?1'bz:1'b0):1'bz ;
-						NS					<= (device_addr_o == slave_addr)?((bitcnt == 8 && scl_pipe==2'b10 )? data_write:register_addr_ack):idle;
-					end
-		data_write: //110
-					begin
-						data_o				<= RW ? 1'bz : (((bitcnt == 0)&& scl_pipe==2'b10 ) ? data_recived[7:0]: data_o) ;
-						//If start condition comes:(Repeat Start)==>Next State change to device_addr 
-						if (sda_pipe==2'b10 && scl_pipe==2'b11) 
-							begin
-							NS					<= device_addr;
-							bitcnt				<= 5'd8;
-							end
-						else
-							NS					<= (bitcnt == 0 && scl_pipe==2'b10 ) ? data_write_ack : data_write ;
-					end
-		data_write_ack: //111
-					begin
-
-						NS					<= ((bitcnt == 8)&& scl_pipe==2'b10 ) ? stop : data_write_ack ;
-						sda_o				<= (device_addr_o == slave_addr)?((bitcnt == 0 && scl_pipe==2'b10 )?1'bz:1'b0):1'bz ;
-					end
-		data_read: //1000
-					begin
-						sda_o				<= (scl_pipe==2'b00)&&(bitcnt>=5'b1)? read_data[bitcnt-1]:sda_o;
-						NS					<= (bitcnt == 0 && scl_pipe==2'b10 ) ? data_read_ack : data_read ;
-					end
-		data_read_ack: //1001
-					begin
-						NS					<= ((bitcnt == 8)&& scl_pipe==2'b10 ) ? stop : data_read_ack ;
-						sda_o				<= (device_addr_o == slave_addr)?1'bz:((bitcnt == 0 && scl_pipe==2'b10 )?1'bz:1'b0) ;
-					end
-		stop: //1010
-					begin
-						NS					<= (sda_pipe==2'b01 && scl_pipe==2'b11) ? idle : stop ;
-						sda_o				<= 1'bz;
-						bitcnt				<= 5'd8;
-					end
+always @(posedge SYSTEM_CLK or negedge RESETn)
+	if(!RESETn)
+		count	<=	5'b0;
+	else if(((scl_pipe== 2'b11)&&(sda_pipe==2'b10))||((scl_pipe== 2'b11)&&(sda_pipe==2'b01)))
+		count	<=	5'b0;
+	else if (scl_pipe==2'b10)
+		count	<=	((count==5'd9))	?	5'b1	:	count	+	1'b1;
 		
-		default:
-					begin
-						device_addr_o		<= 7'dx;
-						register_addr_o		<= 8'dx;
-						data_o   			<= 8'dx;
-						sda_o				<= 1'bz;
-						data_recived		<= 8'bx;
-						RW 					<= 1'bx;
-						bitcnt				<= 5'd8;
+
+
+//////////////////////////////////////////////////////////////////////
+//	 TX  SHIFT
+//////////////////////////////////////////////////////////////////////
+always @(posedge SYSTEM_CLK or negedge RESETn)
+ 	if(!RESETn)
+		shift_reg[7:0]	<=	8'b0;
+	else 
+		begin
+			if(load)
+					shift_reg	<=	tx_data[7:0];
+			else if((tx_shift==1'b1) && (scl_pipe== 2'b10))
+					shift_reg	<=	{shift_reg[6:0],1'b0};
+		end		
+		
+
+
+//////////////////////////////////////////////////////////////////////
+//state machine decode_data tx_shift
+//////////////////////////////////////////////////////////////////////
+	parameter	IDLE		=	4'd1;
+	parameter	ADDRESS		=	4'd2;
+	parameter	RW			=	4'd3;
+	parameter	ADDRESS_ACK	=	4'd4;
+	parameter	OFFSET		=	4'd5;
+	parameter	OFFSET_ACK	=	4'd6;
+	parameter	W_DATA		=	4'd7;
+	parameter	R_DATA		=	4'd8;
+	parameter	ACK_WR		=	4'd9;
+	parameter	ACK_RD		=	4'd10;
+	parameter	WAIT_STOP	=	4'd11;
+	parameter	STOP		=	4'd12;
+
+always @(posedge SYSTEM_CLK or negedge RESETn)
+	if(!RESETn)
+		begin
+			state			<=	IDLE;
+			decode_data		<=	11'b0;
+			tx_shift		<=	1'b0;
+		end
+	else if ((scl_pipe== 2'b11)&&(sda_pipe==2'b10))
+		begin
+			state			<=	/* (state!=IDLE)	?	OFFSET	:	 */IDLE;
+			tx_shift		<=	1'b0;
+		end
+	else if	((scl_pipe== 2'b11)&&(sda_pipe==2'b01))
+		begin
+			state			<=	STOP;
+			tx_shift		<=	1'b0;
+		end
+	else
+		begin
+		case(state)
+			
+			IDLE:	begin
+						state		<=	ADDRESS;
+						decode_data	<=	decode_data;
+						tx_shift	<=	1'b0;
 					end
-		endcase		
-	end	
+			ADDRESS:begin
+						state		<=	(count==5'd9)		?	ADDRESS_ACK		:	ADDRESS;
+						decode_data	<=	(scl_pipe== 2'b01)	?	{decode_data[9:0],SDA}	:	decode_data;
+						tx_shift	<=	1'b0;
+					end					
+			ADDRESS_ACK:	begin
+						state		<=	((address_data[6:0] == slave_addr)&&(scl_pipe== 2'b10))?	((RW_data==1'b0)?OFFSET:R_DATA)
+																															:	ADDRESS_ACK;
+						tx_shift	<=	1'b0;
+					end		
+			
+			OFFSET:	begin
+						state		<=	(count==5'd9)			?	OFFSET_ACK		:	OFFSET;
+						decode_data	<=	(scl_pipe== 2'b01)	?	{decode_data[9:0],SDA}	:	decode_data;
+						tx_shift	<=	1'b0;
+					end	
+			OFFSET_ACK:	begin
+						state		<=	(scl_pipe!= 2'b10)	?	OFFSET_ACK	:	W_DATA;
+						tx_shift	<=	(read)?	1'b1	:	1'b0;			
+					end
+			W_DATA:	begin
+						decode_data	<=	(scl_pipe== 2'b01)	?	{decode_data[9:0],SDA}	:	decode_data;
+						state		<=	(count==5'd9)			?	ACK_WR		:	W_DATA;
+						tx_shift	<=	1'b0;
+					end
+					
+			R_DATA:	begin
+
+						state		<=	(count==5'd9)			?	ACK_RD		:	R_DATA;
+						tx_shift	<=	1'b1;
+					end
+					
+			ACK_WR:	begin
+						decode_data	<= decode_data;
+						tx_shift	<=	1'b0;
+						state		<=	(count==5'd1)		?	WAIT_STOP		:	ACK_WR;
+					end
+			ACK_RD:begin
+						decode_data	<= decode_data;
+						tx_shift	<=	1'b0;
+						state		<=	(count==5'd1)		?	WAIT_STOP		:	ACK_RD;
+					end
+			WAIT_STOP:
+						state		<=	state;
+			STOP:
+						state		<=	state;
+			default:begin
+						state		<=	IDLE;
+						tx_shift	<=	1'b0;	
+					end
+					
+		
+		endcase
+		end
+//////////////////////////////////////////////////////////////////////
+//data-capture
+//////////////////////////////////////////////////////////////////////
+always @(posedge SYSTEM_CLK or negedge RESETn)
+	if(!RESETn)
+		begin
+			address_data	<=	7'b0;
+			RW_data			<=	1'b0;
+			offset_data		<=	8'b0;
+			write_data		<=	8'b0;
+		end
+	else	
+		begin 
+			case(state)
+			 	IDLE	:
+					begin
+						address_data	<=	address_data;
+						RW_data			<=	1'b0;
+						offset_data		<=	offset_data;
+						write_data		<=	8'b0;
+					end 
+				ADDRESS_ACK		:
+					begin
+						address_data	<=	decode_data[7:1];
+						RW_data			<=	decode_data[0];
+					end				
+			
+				OFFSET_ACK	:
+					begin
+						offset_data		<=	decode_data;
+					end				
+			
+				ACK_WR	:
+					begin
+						write_data		<=	decode_data;
+						offset_data		<=	offset_data;
+					end	
+				STOP	:
+					begin
+						address_data	<=	address_data;
+						RW_data			<=	RW_data;
+						offset_data		<=	offset_data;
+						write_data		<=	write_data;
+					end	
+				default:
+					begin
+						address_data	<=	address_data;
+						RW_data			<=	RW_data;
+						offset_data		<=	offset_data;
+						write_data		<=	write_data;
+					end
+			endcase
+		end
+
+
+
+
+//////////////////////////////////////////////////////////////////////
+//	 TX  SDA_o
+//////////////////////////////////////////////////////////////////////
+always @(posedge SYSTEM_CLK or negedge RESETn)
+	if(!RESETn)
+		sda_o	<=	1'bz;
+	else if ((scl_pipe== 2'b11)&&(sda_pipe==2'b10))
+		sda_o	<=	1'bz;
+	else if	((scl_pipe== 2'b11)&&(sda_pipe==2'b01))
+		sda_o	<=	1'bz;
+	else	
+		begin 
+			if (state[4:0] == ADDRESS_ACK)
+				sda_o	<=	(address_data[6:0] == slave_addr)	?	1'b0	:	1'bz;
+			else if (state[4:0] == OFFSET_ACK)
+				sda_o	<=	((count==5'd9)) ? 1'b0 : 1'bz;
+			else if	(state[4:0] == ACK_WR)
+				sda_o	<=	(count==5'd9) ? 1'b0 : 1'bz;
+			else if	(state[4:0] == ACK_RD)
+				sda_o	<=	1'bz;
+ 			else if	(tx_shift)	 
+				sda_o	<=	shift_reg[7]; 
+			else
+				sda_o	<=	1'bz;
+		end	
+//////////////////////////////////////////////////////////////////////
+//	Decode Data and Register W/R enable
+//////////////////////////////////////////////////////////////////////
+	assign	rx_address	=	address_data[6:0];
+	assign	rx_data		=	write_data	[7:0];
+	assign	rx_offset	=	offset_data	[7:0];
+	assign	owrite_en	=	((state[4:0] == ACK_WR/* WAIT_STOP */	)	&&	write	)?	1'b1:1'b0;
+	assign	oread_en	=	((state[4:0] == ADDRESS_ACK	)	&&	read	)?	1'b1:1'b0;
+	assign	SDA		=	sda_o;
+
 endmodule
