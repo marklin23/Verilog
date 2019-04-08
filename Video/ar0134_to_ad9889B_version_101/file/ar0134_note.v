5f2c8d3e2fbce0f1f77455fd121f6642b35763b2
Mark Lin <marklin23@hotmail.com>
2019-04-08 11:40:28 +0800

    add file

---
+++ Video/ar0134_to_ad9889B_version_101/file/ar0134_note.v
@@ -0,0 +1,13 @@
+AR0134 note
+
+1. Aptina's AR0134 sensor is capable of a maximum resolution of 1280 x 960 at up to 54 fps
+
+2. By convention, the sensor core pixel array is shown with the first addressable (logical)
+   pixel (0,0) in the top right corner
+
+3. FV will be asserted for an integral number of row times, which will normally be equal to
+   the height of the output image.
+4. LV will be asserted during the valid pixels of each row
+
+5. Normally, LV will only be asserted if FV is asserted;
+Table 18: Test Pattern Modes
